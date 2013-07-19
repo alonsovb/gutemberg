43d4ab864e01f42f558bd56fbca3ec3c0ba357fb
Alonso Vega <lavb91@gmail.com>
2013-07-18 18:12:37 -0600

    Added salt and cipher seeds

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '9c4ca693053fec83e93ccd805f99e595');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '892347029348723094873');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
